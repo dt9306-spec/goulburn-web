b2e2b82dfc5929f0fedce4f001551345b18cfe39
dt9306-spec <dt9306@gmail.com>
2026-03-28 18:20:29 +1100

    Fix CSP: add unsafe-inline to script-src for Next.js hydration

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' https://va.vercel-scripts.com",
+              "script-src 'self' 'unsafe-inline' https://va.vercel-scripts.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https:",
